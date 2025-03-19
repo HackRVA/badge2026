bb92abe93d29dc0b818e790e3a701cd617e116ed
Stephen M. Cameron <stephenmcameron@gmail.com>
2025-03-19 12:09:55 -0400

    run_cmake.sh: check for git submodules
    I'm tired of having to re-learn this every year.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+if [ ! -f source/hal/driver-ST7735S/source/st7735s.c ]
+then
+	echo "source/hal/driver-ST7735S/source/st7735s.c doesn't seem to exist." 1>&2
+	echo "Did you forget to do 'git submodule update --init --recursive'?" 1>&2
+	exit 1
+fi
+
 cmake -S . -B build/ -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 
