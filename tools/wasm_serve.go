401285bb345972fc225cd264adc15dca8d2ef3b8
Dustin Firebaugh <dafirebaugh@gmail.com>
2025-04-05 20:02:50 -0400

    initial wasm build

---
+++ tools/wasm_serve.go
@@ -0,0 +1,81 @@
+// wasm_serve is just a simple http file server with headers set to work with pthreads in wasm
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func copyDir(src, dst string) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(dst, relPath)
+
+		if info.IsDir() {
+			return os.MkdirAll(destPath, os.ModePerm)
+		}
+
+		srcFile, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+
+		destFile, err := os.Create(destPath)
+		if err != nil {
+			return err
+		}
+		defer destFile.Close()
+
+		_, err = io.Copy(destFile, srcFile)
+		return err
+	})
+}
+
+func main() {
+	port := "8080"
+
+	srcDir := flag.String("src", "", "Source directory to copy from (optional)")
+	destDir := flag.String("dest", "./build_wasm/source/", "Destination directory to copy to (used for serving)")
+
+	flag.Parse()
+
+	if *srcDir != "" {
+		fmt.Println("Copying files...")
+		err := copyDir(*srcDir, *destDir)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error copying files: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("Files copied successfully.")
+	} else {
+		fmt.Println("No source directory specified, skipping copy.")
+	}
+
+	fs := http.FileServer(http.Dir(*destDir))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
+		w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
+		fs.ServeHTTP(w, r)
+	})
+
+	fmt.Printf("Serving on http://localhost:%s\n", port)
+	err := http.ListenAndServe("0.0.0.0:"+port, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
+		os.Exit(1)
+	}
+}
