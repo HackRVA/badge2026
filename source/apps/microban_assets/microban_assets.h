d1396c26d42d04299a2b271d4a795c592fd3547e
differenceclouds <differenceclouds@gmail.com>
2025-04-28 21:12:51 -0400

    microban 1.0

---
+++ source/apps/microban_assets/microban_assets.h
@@ -0,0 +1,12 @@
+#ifndef MICROBAN_ASSETS_H__
+#define MICROBAN_ASSETS_H__
+
+#include "eb_bg.h"
+#include "eb_dude.h"
+#include "chip_16px.h"
+#include "redblocks.h"
+#include "microban_levels.h"
+#include "microban_levels_rects.h"
+
+#endif
+
