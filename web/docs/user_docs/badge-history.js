15b437e40d1c10004c6fd5265babecd5dce96a7c
Dustin Firebaugh <dafirebaugh@gmail.com>
2025-05-06 17:55:38 -0400

    web: first pass at badge gallery

---
+++ web/docs/user_docs/badge-history.js
@@ -0,0 +1,38 @@
+class GalleryImage extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.src = this.getAttribute('src') || '';
+		this.label = this.getAttribute('label') || '';
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.shadowRoot.innerHTML = `
+          <style>
+            figure {
+              margin: 0;
+              text-align: center;
+            }
+            img {
+              width: 100%;
+              height: auto;
+              border-radius: 4px;
+            }
+            figcaption {
+              margin-top: 0.5em;
+              font-weight: bold;
+            }
+          </style>
+          <figure>
+            <figcaption>${this.label}</figcaption>
+            <img src="${this.src}" alt="${this.label}" />
+          </figure>
+        `;
+	}
+}
+
+customElements.define('gallery-image', GalleryImage);
--- web/docs/user_docs/src/SUMMARY.md
+++ web/docs/user_docs/src/SUMMARY.md
@@ -3,5 +3,6 @@
 - [About](./intro.md)
 - [Simulator](./sim.md)
 - [Hardware](./hardware.md)
+- [History](./history.md)
 - [Software](./software.md)
 - [HackRVA](./hackrva.md)
