6f0440f7a30e9303f34d96a8194f57e1211235ef
Stephen M. Cameron <stephenmcameron@gmail.com>
2025-05-14 09:03:38 -0400

    badgey: fix some broken treasures and clues
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- source/apps/badgey.c
+++ source/apps/badgey.c
@@ -3039,11 +3039,13 @@ static struct treasure_clue {
 	{ "\nFROM SURSEE\nHEAD 2 EAST\n3 SOUTH\nAND DIG\nFOR THE\nDPAD", &NW42, 10, -1, -1, clue_type_pub },
 	{ "\nFROM SURSEE\nHEAD 2 EAST\n3 SOUTH\nAND DIG\nFOR THE\nDPAD", &NW42, 10, -1, -1, clue_type_temple },
 	/* clues in KALFO, (NW42, town 13) */
-	{ "\nDIG AROUND\nBEHIND TECH NOIR\nIN THE TOWN\nOF CALEV\n", &NW42, 13, -1, -1, clue_type_pub },
-	{ "\nDIG AROUND\nBEHIND TECH NOIR\nIN THE TOWN\nOF CALEV\n", &NW42, 13, -1, -1, clue_type_rando },
+	{ "\nDIG AROUND\nBEHIND SCOOTERS\nIN THE TOWN\nOF CALEV\n", &NW42, 13, -1, -1, clue_type_pub },
+	{ "\nDIG AROUND\nBEHIND SCOOTERS\nIN THE TOWN\nOF CALEV\n", &NW42, 13, -1, -1, clue_type_rando },
 	/* clues in BURNIP, (NW42, town 14) */
-	{ "\nDIG AROUND\nBEHIND SIGTRAP\nIN THE TOWN\nOF NORJIG\n", &NW42, 14, -1, -1, clue_type_rando },
-	{ "\nDIG AROUND\nBEHIND SIGTRAP\nIN THE TOWN\nOF NORJIG\n", &NW42, 14, -1, -1, clue_type_pub },
+	{ "\nDIG AROUND\nBEHIND RED\nDWARF BAR\nIN THE TOWN\nOF NORJIG\n",
+			&NW42, 14, -1, -1, clue_type_rando },
+	{ "\nDIG AROUND\nBEHIND RED\nDWARF BAR\nIN THE TOWN\nOF NORJIG\n",
+			&NW42, 14, -1, -1, clue_type_pub },
 	/* clues in , (BORTON, town 21 JARLS) */
 	{ "\nDIG AROUND\nBEHIND THE INN\nIN THE TOWN\nOF LAKNIV\n", &borton, 21, -1, -1, clue_type_rando },
 	{ "\nDIG AROUND\nBEHIND THE INN\nIN THE TOWN\nOF LAKNIV\n", &borton, 21, -1, -1, clue_type_pub },
@@ -3051,8 +3053,8 @@ static struct treasure_clue {
 	{ "\nSEARCH\nSMUGGLERS CAVE\nDIG AT 2,62\n", &borton, 22, -1, -1, clue_type_rando },
 	{ "\nSEARCH\nSMUGGLERS CAVE\nDIG AT 2,62\n", &borton, 22, -1, -1, clue_type_pub },
 	/* clues in gnarg, jalta */
-	{ "\nEXPLORE\nEVO MINES\nDIG AT 44,36\n", &gnarg, 40, -1, -1, clue_type_rando },
-	{ "\nSEEK WITHIN\nEVO MINES\nDIG AT 44,36\n", &gnarg, 40, -1, -1, clue_type_pub },
+	{ "\nEXPLORE\nEVO MINES\nDIG AT 55,62\n", &gnarg, 40, -1, -1, clue_type_rando },
+	{ "\nSEEK WITHIN\nEVO MINES\nDIG AT 55,62\n", &gnarg, 40, -1, -1, clue_type_pub },
 	/* clues in gnarg, yarnow */
 	{ "\nDELVE INTO\nSPIDER CAVE\nEXCAVATE x, y\n", &gnarg, 44, -1, -1, clue_type_rando },
 	{ "\nSPELUNK IN\nSPIDER CAVE\nDIG AT x, y\n", &gnarg, 44, -1, -1, clue_type_pub },
@@ -3832,17 +3834,17 @@ static void setup_static_treasures(void)
 	add_static_treasure(&ossaria, -1, 10, 10, 1000, -1, CHEST_STATUS_BURIED);
 	/* If you add more static treasures, change NUM_STATIC_CHESTS value */
 	add_static_treasure(&ossaria, -1, 40, 3, 200, LED_SCREEN, CHEST_STATUS_BURIED); /* ossaria, on an island */
-	add_static_treasure(&gnarg, 7, 5, 44, 200, SOLDER, CHEST_STATUS_BURIED); /* ossaria, klon caverns */
+	add_static_treasure(&ossaria, 7, 5, 44, 200, SOLDER, CHEST_STATUS_BURIED); /* ossaria, klon caverns */
 	add_static_treasure(&NW42, -1, 10, 10, 200, PLASTIC_DPAD, CHEST_STATUS_BURIED); /* NW42, NEAR SURSEE */
 	add_static_treasure(&NW42, 11, 11, 12, 200, RP2040CHIP, CHEST_STATUS_BURIED); /* NW42, CALEV */
 	add_static_treasure(&NW42, 12, 11, 51, 200, CIRCUIT_BOARD, CHEST_STATUS_BURIED); /* NW42, NORJIG, SIGTRAP */
-	add_static_treasure(&borton, 13, 24, 29, 200, SMALL_SPEAKER, CHEST_STATUS_BURIED); /* borton, LAKNIV, */
+	add_static_treasure(&borton, 23, 34, 29, 200, SMALL_SPEAKER, CHEST_STATUS_BURIED); /* borton, LAKNIV, */
 	add_static_treasure(&borton, 26, 2, 62, 200, AMP_CHIP, CHEST_STATUS_BURIED); /* borton, smuggler's cave, */
 	add_static_treasure(&skang, 36, 1, 42, 200, A_BUTTON, CHEST_STATUS_BURIED); /* skang, tunnels/doom, */
 	add_static_treasure(&skang, 32, 45, 42, 200, RESET_BUTTON, CHEST_STATUS_BURIED); /* skang, torxun */
-	add_static_treasure(&gnarg, 49, 36, 62, 200, B_BUTTON, CHEST_STATUS_BURIED); /* gnarg, spider cave */
+	add_static_treasure(&gnarg, 49, 37, 62, 200, B_BUTTON, CHEST_STATUS_BURIED); /* gnarg, spider cave */
 	add_static_treasure(&gnarg, 44, 34, 40, 200, USB_CONNECTOR, CHEST_STATUS_BURIED); /* gnarg, yarnow */
-	add_static_treasure(&gnarg, 45, 44, 36, 200, BATTERY, CHEST_STATUS_BURIED); /* gnarg, evo mines */
+	add_static_treasure(&gnarg, 45, 55, 62, 200, BATTERY, CHEST_STATUS_BURIED); /* gnarg, evo mines */
 }
 
 static void badgey_init(void)
