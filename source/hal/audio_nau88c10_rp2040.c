09b23ad44d94efa4e0bab07c83ad1a5839b7b5ff
Peter Maxwell Warasila <madmaxwell@soundcomesout.com>
2025-05-17 19:24:35 -0400

    audio: fix beeps not stopping when duration and frequency are zero
    Signed-off-by: Peter Maxwell Warasila <madmaxwell@soundcomesout.com>

--- source/hal/audio_nau88c10_rp2040.c
+++ source/hal/audio_nau88c10_rp2040.c
@@ -144,6 +144,10 @@ static void prv_audio_i2s_process(int32_t *in, int32_t *out)
             if (++(beep->elapsed_ms) == beep->duration_ms) {
                 prv_audio_out_beep_complete(beep);
             }
+        } else {
+            for (size_t i = 0; i < STEREO_BUFFER_SIZE; i += 2) {
+                out[i] = 0U;
+            }
         }
     } else {
         /* Nothing is playing. */
@@ -265,11 +269,13 @@ int audio_in_cb_count()
 int audio_out_beep_with_cb(uint16_t freq_hz, uint16_t dur_ms, void (*cb)(void))
 {
     uint32_t period;
+    enum audio_out_mode_ out_mode = AUDIO_OUT_MODE_OFF;
     if ((freq_hz == 0) && (dur_ms == 0)) {
         /* Cancel the current beep. */
         period = UINT16_MAX;
     } else if (freq_hz == 0 && cb != NULL) { 
         /* We're being asked to play a rest. */
+        out_mode = AUDIO_OUT_MODE_BEEP;
         period = UINT16_MAX;
     } else if ((freq_hz < AUDIO_BEEP_FREQ_HZ_MIN)
                || (freq_hz > AUDIO_BEEP_FREQ_HZ_MAX)
@@ -278,12 +284,13 @@ int audio_out_beep_with_cb(uint16_t freq_hz, uint16_t dur_ms, void (*cb)(void))
     {
         return -1;
     } else {
-       period = AUDIO_FS / freq_hz;
+        out_mode = AUDIO_OUT_MODE_BEEP;
+        period = AUDIO_FS / freq_hz;
     }
 
     audio_stby_ctl(false);
     uint32_t irqs = save_and_disable_interrupts(); // FIXME: irq locking insufficient with multiple cores. -PMW
-    m_audio_out_mode = AUDIO_OUT_MODE_BEEP;
+    m_audio_out_mode = out_mode;
     m_audio_out_beep.duration_ms = dur_ms;
     m_audio_out_beep.elapsed_ms = 0;
     if (m_audio_out_beep.period != period) {
