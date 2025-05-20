976285bef57709780c699b46ee4890cbb26c3ea7
Dustin Firebaugh <dafirebaugh@gmail.com>
2025-05-20 18:09:15 -0400

    cmake: adding guard to check for the other submodule

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ ! -f source/hal/i2s/rp2040_i2s_example/i2s.c ]
+then
+	echo "source/hal/i2s/rp2040_i2s_example/i2s.c doesn't seem to exist." 1>&2
+	echo "Did you forget to do 'git submodule update --init --recursive'?" 1>&2
+	exit 1
+fi
+
 if [ ! -f source/hal/driver-ST7735S/source/st7735s.c ]
 then
 	echo "source/hal/driver-ST7735S/source/st7735s.c doesn't seem to exist." 1>&2
