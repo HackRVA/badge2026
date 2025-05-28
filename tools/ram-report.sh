6351c95d0ef042a264621b0a0066204924eeb8b3
Stephen M. Cameron <stephenmcameron@gmail.com>
2025-05-28 10:30:35 -0400

    ram-report.sh: add total to summary report
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- tools/ram-report.sh
+++ tools/ram-report.sh
@@ -4,15 +4,18 @@ if [ "$1" = "-s" ]
 then
 	nm -S -l -t d --size-sort source/badge2025_c | grep ' [bBgGdD] ' | sed -e 's/:.*$//' |\
 	awk '
+		BEGIN { total = 0 }
 		{
 			k = $5;
 			if (k == "")
 				k = $4 " (static local)";
 			ram[k] += $2;
+			total += $2;
 		}
 		END { for (key in ram) {
 			printf("%d %s\n", ram[key], key);
 		      }
+		      printf("%d Total\n", total);
 		}' | sort -n
 else
 	nm -S -l -t d --size-sort source/badge2025_c | grep ' [bBgGdD] '
