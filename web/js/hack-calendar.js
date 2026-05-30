084b5104ec371b586ca42f48922100620040eaf0
Dustin Firebaugh <dafirebaugh@gmail.com>
2026-05-30 13:42:42 -0400

    web: migrate map-locator and hack-calendar to native custom elements

--- web/js/hack-calendar.js
+++ web/js/hack-calendar.js
@@ -1,24 +1,30 @@
+class HackCalendar extends HTMLElement {
+	constructor() {
+		super();
+		this.calendars = [
+			{ calendarId: "hackrva.org_fihosi84pjcuriel6b1ba4qmo8@group.calendar.google.com" },
+			{
+				calendarId: "1qc2m9vm31ekf46t3dkls0j84e62fihq@import.calendar.google.com",
+				color: "#B241D1"
+			},
+		];
+	}
 
-export default function HackCalendar(ctx) {
-  const calendars = [
-    { calendarId: "hackrva.org_fihosi84pjcuriel6b1ba4qmo8@group.calendar.google.com" },
-    {
-      calendarId: "1qc2m9vm31ekf46t3dkls0j84e62fihq@import.calendar.google.com",
-      color: "#B241D1"
-    },
-  ];
+	connectedCallback() {
+		this.render();
+	}
 
-  return () => {
-    const calParams = calendars.map(({ calendarId, color }) => {
-      let p = `src=${encodeURIComponent(calendarId)}`;
-      if (color) p += `&color=${encodeURIComponent(color)}`;
-      return p;
-    });
+	render() {
+		const calParams = this.calendars.map(({ calendarId, color }) => {
+			let p = `src=${encodeURIComponent(calendarId)}`;
+			if (color) p += `&color=${encodeURIComponent(color)}`;
+			return p;
+		});
 
-    const qs = calParams.join("&") + "&ctz=America/New_York";
-    const src = `https://calendar.google.com/calendar/embed?${qs}`;
+		const qs = calParams.join("&") + "&ctz=America/New_York";
+		const src = `https://calendar.google.com/calendar/embed?${qs}`;
 
-    return `
+		this.innerHTML = `
       <div>
         <iframe
           src="${src}"
@@ -30,6 +36,7 @@ export default function HackCalendar(ctx) {
         </iframe>
       </div>
     `;
-  };
+	}
 }
 
+customElements.define('hack-calendar', HackCalendar);
--- web/js/map-locator.js
+++ web/js/map-locator.js
@@ -1,26 +1,36 @@
-export default function MapLocator(ctx) {
-  let address = ctx.attributes.address || "2026A Dabney Rd, Richmond, VA 23230";
+class MapLocator extends HTMLElement {
+	static get observedAttributes() {
+		return ["address"];
+	}
 
-  function getMapSrc(addr) {
-    const encodedAddress = encodeURIComponent(addr);
-    return `https://maps.google.com/maps?width=100%25&height=600&hl=en&q=${encodedAddress}&t=&z=14&ie=UTF8&iwloc=B&output=embed`;
-  }
+	connectedCallback() {
+		this.render();
+	}
 
-  ctx.onAttributeChanged((name, oldValue, newValue) => {
-    if (name === "address") {
-      address = newValue;
-      ctx.update();
-    }
-  });
+	attributeChangedCallback() {
+		if (this.isConnected) {
+			this.render();
+		}
+	}
 
-  return () => `
-    <iframe
-      class="calendar"
-      width="100%"
-      height="300"
-      title="hackrva-google-maps-locator"
-      src="${getMapSrc(address)}"
-    ></iframe>
-  `;
+	getMapSrc(addr) {
+		const encodedAddress = encodeURIComponent(addr);
+		return `https://maps.google.com/maps?width=100%25&height=600&hl=en&q=${encodedAddress}&t=&z=14&ie=UTF8&iwloc=B&output=embed`;
+	}
+
+	render() {
+		const address = this.getAttribute("address") || "2026A Dabney Rd, Richmond, VA 23230";
+
+		this.innerHTML = `
+	    <iframe
+	      class="calendar"
+	      width="100%"
+	      height="300"
+	      title="hackrva-google-maps-locator"
+	      src="${this.getMapSrc(address)}"
+	    ></iframe>
+	  `;
+	}
 }
 
+customElements.define('map-locator', MapLocator);
